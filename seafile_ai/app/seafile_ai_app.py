36e10c031c67299f507ea5968dc9f1775567d206
zheng.shen <zheng.shen@seafile.com>
2024-12-13 08:42:12 +0000

    optimize doc ocr

--- seafile_ai/app/seafile_ai_app.py
+++ seafile_ai/app/seafile_ai_app.py
@@ -15,6 +15,12 @@ class SeafileAIApp(object):
 
         self.image_tags_api = ImageTagsAPI(config.IMAGE_TAGS_SERVICE_URL, config.IMAGE_TAGS_SERVICE_KEY)
 
+        if config.OCR_SERVICE_TYPE == 'seafile-ocr':
+            from seafile_ai.utils.seafile_ocr_api import SeafileOCRAPI
+            self.ocr_api = SeafileOCRAPI(config.OCR_SERVICE_URL, config.OCR_SERVICE_KEY)
+        else:
+            raise Exception('unknown ocr service type')
+
         self.text_processing_manager = TextProcessingManager(self, config.LLM_TYPE)
         self.image_processing_manager = ImageProcessingManager(self)
         self.seafile_ai_http_server = SeafileAIHttpServer(self)
