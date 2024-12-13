51aa52b0c8ea8b16e7110761eef412b41b88497e
zheng.shen <zheng.shen@seafile.com>
2024-12-13 15:36:27 +0800

    update

--- seafile_ai/image_processing/image_processing_manager.py
+++ seafile_ai/image_processing/image_processing_manager.py
@@ -22,7 +22,7 @@ class ImageProcessingManager:
                 "content": [
                     {
                         "type": "text",
-                        "text": f"Please describe the contents of this picture in Chinese. Focus solely on the objects and details depicted, without discussing the emotions the picture may evoke. The description should be approximately 100 words."
+                        "text": f"Please describe the contents of this picture in {LANGUAGE[lang]}. Focus solely on the objects and details depicted, without discussing the emotions the picture may evoke. The description should be approximately 100 words."
                     },
                     {
                         "type": "image_url",
