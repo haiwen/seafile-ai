3726b02e660e3b55056192ce64b75af712552f9c
zheng.shen <zheng.shen@seafile.com>
2024-12-13 14:27:07 +0800

    optimize_image_desc_prompt

--- seafile_ai/image_processing/image_processing_manager.py
+++ seafile_ai/image_processing/image_processing_manager.py
@@ -22,7 +22,7 @@ class ImageProcessingManager:
                 "content": [
                     {
                         "type": "text",
-                        "text": f"Please describe this image in {LANGUAGE[lang]} with a sentence of about 100 words."
+                        "text": f"Please describe the contents of this picture in {LANGUAGE[lang]} one by one. Be careful not to describe the feeling the picture gives people. The word count should be about 100 words."
                     },
                     {
                         "type": "image_url",
