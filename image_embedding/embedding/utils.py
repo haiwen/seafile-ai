f762281360c5ad8d7a13f962dc32ab4bee3edc94
cir9no <44470218+cir9no@users.noreply.github.com>
2025-01-20 18:32:27 +0800

    chore: optimize the proportion of faces in images

--- image_embedding/embedding/utils.py
+++ image_embedding/embedding/utils.py
@@ -6,11 +6,18 @@ def get_face_image(image, box):
     height, width, _ = image.shape
     face_height = box[3] - box[1]
     face_width = box[2] - box[0]
-    left = max(box[0] - int(face_width * 0.25), 0)
-    top = max(box[1] - int(face_height * 0.25), 0)
-    right = min(box[2] + int(face_width * 0.25), width)
-    bottom = min(box[3] + int(face_height * 0.25), height)
+    face_size = max(face_height, face_width)
+    
+    left = max(box[0] - int(face_size * 0.5), 0)
+    top = max(box[1] - int(face_size * 0.5), 0)
+    right = min(box[2] + int(face_size * 0.5), width)
+    bottom = min(box[3] + int(face_size * 0.5), height)
+    
     face_img = image[top:bottom, left:right]
+    
+    # Resize the face image to a square
+    face_img = cv2.resize(face_img, (face_size, face_size))
+    
     face_img = cv2.cvtColor(face_img, cv2.COLOR_RGB2BGR)
     _, encoded_img = cv2.imencode('.jpg', face_img)
     byte_io = io.BytesIO(encoded_img.tobytes())
