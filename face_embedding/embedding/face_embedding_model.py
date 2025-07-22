46a4ad3ded364f7bdccd6f00ea8effab90a6094a
jinlong.sun <jinlong.sun@seafile.com>
2025-07-22 09:59:50 +0000

    heic-support

--- face_embedding/embedding/face_embedding_model.py
+++ face_embedding/embedding/face_embedding_model.py
@@ -1,25 +1,43 @@
 import base64
-
+import io
+import logging
 import cv2
 import numpy as np
+import pillow_heif
+
+from PIL import Image
 from sklearn import preprocessing
 
 from face_embedding.embedding.utils import get_face_image
 from face_embedding.models.model import Model
 
+pillow_heif.register_heif_opener()
+
+logger = logging.getLogger(__name__)
 
 class FaceEmbeddingModel:
     def __init__(self, model_dir, gpu_id=0):
         self.gpu_id = gpu_id
         self.model = Model(model_dir, self.gpu_id)
 
+    def image_preprocess(self, content):
+        image = cv2.imdecode(np.frombuffer(content, dtype=np.uint8), 1)
+        if image is not None:
+            return cv2.cvtColor(image, cv2.COLOR_BGR2RGB)
+        try:
+            image = Image.open(io.BytesIO(content))
+            image = image.convert('RGB')
+            return np.array(image)
+        except Exception as e:
+            logger.error(f'Face image preprocess error: {e}')
+            return None
+            
     def embedding(self, content, need_face):
         result = []
-        input_image = cv2.imdecode(np.frombuffer(content, dtype=np.uint8), 1)
+        input_image = self.image_preprocess(content)
         if input_image is None:
             return []
-
-        input_image = cv2.cvtColor(input_image, cv2.COLOR_BGR2RGB)
+        
         faces = self.model.get(input_image)
         for face in faces:
             det_score = face.det_score
@@ -36,7 +54,7 @@ class FaceEmbeddingModel:
                 box = face.bbox.astype(np.int64)
                 face_image = get_face_image(input_image, box)
                 face_info['face'] = base64.b64encode(face_image).decode('utf-8')
-
+            
             result.append(face_info)
-
+        
         return result
