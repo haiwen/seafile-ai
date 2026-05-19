9913c098ff46e9aa6aeab96e5b3728a9876cc64d
jiwei.ran <jiwei.ran@seafile.com>
2026-05-19 05:43:22 +0000

    Refactor and enhance image processing, authentication, and logging

--- seafile_ai/image_processing/utils.py
+++ seafile_ai/image_processing/utils.py
@@ -1,11 +1,18 @@
 from io import BytesIO
-
+import base64
 from PIL import Image
 import pillow_heif
+
+from seafile_ai.repo_metadata.constants import FACES_TABLE
 pillow_heif.register_heif_opener()
 
 
-
+VECTOR_DEFAULT_FLAG = '0'
+FACE_EMBEDDING_DIM = 512
+FACES_TMP_DIR = '/tmp'
+FACES_SAVE_PATH = '_Internal/Faces'
+EMBEDDING_UPDATE_LIMIT = 200
+SUPPORTED_IMAGE_FORMATS = ('jpeg', 'jpg', 'heic', 'png', 'bmp', 'tif', 'tiff', 'jfif', 'jpe', 'ppm', 'heic')
 def resize_image_binary(image_binary):
     
     img = Image.open(BytesIO(image_binary))
@@ -25,3 +32,44 @@ def resize_image_binary(image_binary):
     output_buffer.seek(0)
 
     return output_buffer.getvalue()
+
+def feature_distance(feature1, feature2):
+    import numpy as np
+    diff = np.subtract(feature1, feature2)
+    dist = np.sum(np.square(diff), 0)
+    return dist
+
+
+def b64encode_embeddings(embeddings):
+    import numpy as np
+    embedding_array = np.array(embeddings).astype(np.float32)
+    encode = base64.b64encode(embedding_array.tobytes())
+    return encode.decode('utf-8')
+
+
+def b64decode_embeddings(encode):
+    import numpy as np
+    decode = base64.b64decode(encode)
+    embedding_array = np.frombuffer(decode, dtype=np.float32)
+    face_num = len(embedding_array) // FACE_EMBEDDING_DIM
+    embedding = embedding_array.reshape((face_num, FACE_EMBEDDING_DIM)).tolist()
+    return embedding
+
+
+def get_cluster_by_center(center, clusters):
+    min_distance = float('inf')
+    nearest_cluster = None
+    for cluster in clusters:
+        vector = cluster.get(FACES_TABLE.columns.vector.name)
+        if not vector:
+            continue
+
+        vector = b64decode_embeddings(vector)[0]
+        distance = feature_distance(center, vector)
+        if distance < 1 and distance < min_distance:
+            min_distance = distance
+            nearest_cluster = cluster
+    return nearest_cluster, min_distance
+
+def get_min_cluster_size(faces_num):
+    return max(faces_num // 100, 5)
